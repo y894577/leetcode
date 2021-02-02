033c010b0ce1fec2239e3c523606c54eb80e2494
y894577 <44811269+y894577@users.noreply.github.com>
2021-02-02 19:41:53 +0800

    Update t105.java

--- src/Algorithms3/t105.java
+++ src/Algorithms3/t105.java
@@ -2,6 +2,26 @@ package Algorithms3;
 
 public class t105 {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        TreeNode root = new TreeNode();
+
+        return root;
+    }
+
+    public void find(int[] preorder, int[] inorder, int mid, TreeNode root) {
+        //前序遍历找出跟节点
+        for (int i = 0; i < preorder.length; i++) {
+            int preNode = preorder[i];
+            root.val = preNode;
+            int point = -1;
+            for (int j = 0; j < inorder.length; j++) {
+                if (preNode == inorder[j]) {
+                    find(preorder, inorder, j, root);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
 
     }
 }
